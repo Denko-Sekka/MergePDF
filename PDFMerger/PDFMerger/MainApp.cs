ec2256da5e02c12046102b071dd5a45dc79389ce
gdioni <gdioni@mafindustries.com>
2019-09-27 11:55:03 -0700

    Dev #2: added pluginloader

--- PDFMerger/PDFMerger/MainApp.cs
+++ PDFMerger/PDFMerger/MainApp.cs
@@ -27,6 +27,5 @@ namespace PDFMerger
                 flpMain.Controls.Add((UserControl)item.UC);
             }
         }
-        // TODO - the main app and plugin contract has been added. add a way to read the plugins from a path
     }
 }
