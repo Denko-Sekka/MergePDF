e14e0fcb6a2cb93a9f0ce7b60b633adbcc7d9533
gdioni <gdioni@mafindustries.com>
2019-09-27 11:19:19 -0700

    Development #1: - added plugin interface - added main-app - added plugins dictionary

---
+++ PDFMerger/PDFMerger/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFMerger
+{
+    public partial class MainApp : Form
+    {
+        private readonly Dictionary<string, IPlugin> _plugins;
+        public MainApp()
+        {
+            InitializeComponent();
+            _plugins = new Dictionary<string, IPlugin>();
+            
+
+        }
+        // TODO - the main app and plugin contract has been added. add a way to read the plugins from a path
+    }
+}
